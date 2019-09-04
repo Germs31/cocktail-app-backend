8275a4ea6640deb0d86f89825058ea7d2e82b51b
German Nunez <germannunez@Germans-MacBook-Pro.local>
2019-09-04 10:17:57 -0700

    auth controller setup

---
+++ controller/auth.js
@@ -0,0 +1,67 @@
+const express = require('express')
+const router = express.Router()
+const User    = require('../models/users')
+const bcrypt  = require('bcryptjs')
+
+router.post('/login', async (req,res)=>{
+    try{
+
+        const foundUser = await User.findOne({username: req.body.username});
+        console.log(foundUser, '<--- foundUser');
+
+        if(foundUser){
+            if(bcrypt.compareSync(req.body.password, foundUser.password)){
+                req.session.userId = foundUser._id;
+                req.session.username = foundUser.username;
+                req.session.logged = true;
+
+                res.redirect('/auth')
+            }else {
+
+                req.session.message = 'Username or Password incorrect';
+                res.redirect('/');
+          
+            }
+
+        }else{
+
+            req.session.message = 'Username or Password incorrect';
+            res.redirect('/');
+
+        }
+
+    }catch(err){
+        console.log(err)
+        res.send(err);
+    }
+})
+
+router.post('/register', async (req,res) =>{
+
+    const password = req.body.password;
+
+    const hashedPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+    console.log(hashedPassword)
+    req.body.password = hashedPassword;
+
+    try{
+
+        const createdUser = await User.create(req.body);
+        console.log(createdUser, '<--- created user');
+
+        req.session.userId = createdUser._id;
+        req.session.username = createdUser.username;
+        req.session.logged = true;
+
+        res.redirect('/auth')
+
+    }catch(err){
+
+        console.log(err)
+        res.send(err)
+
+    }
+})
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const app = express();
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const cors = require('cors')
+
+
+
 const PORT = 3000
+
 const cocktailRouter = require('./controller/cocktail.js')
+const authRouter = require('./controller/auth.js')
+const userRouter = require('./controller/user.js')
+
+
 require('./db/db');
 require('dotenv').config()
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(session({
+    secret: 'muckduck',
+    resave: false,
+    saveUninitialized: false
+}));
+
+
+
+app.use(cors());
+
 app.use('/cocktail', cocktailRouter)
+app.use('/auth', authRouter)
+app.use('/user', userRouter)
 
 
 app.listen(PORT, (err) => {
